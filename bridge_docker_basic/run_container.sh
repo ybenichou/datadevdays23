7ff8f5e0465fa69f9239673810646cfab8fb1ee6
ybenichou <122894265+ybenichou@users.noreply.github.com>
2024-01-28 11:49:50 +0200

    Update run_container.sh

--- bridge_docker_basic/run_container.sh
+++ bridge_docker_basic/run_container.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-export TOKEN_SECRET="<...>"
+export TOKEN_SECRET="iIt9jjvBRZ6v6Za7sikgFQ==:w4TVeF3BwkGCco6yg3XzsH4EnMtDO6T4"
 
-docker run -d  --name "<bridge-container-name>" \
-  -e AGENT_NAME="<bridge-agent-name>" \
-  -e TC_SERVER_URL="<https://prod-useast-a.online.tableau.com>" \
-  -e SITE_NAME="<sitename>" \
-  -e USER_EMAIL="<user@company.com>" \
-  -e TOKEN_ID="<token-id>" \
+docker run -d  --name "bridge_basic" \
+  -e AGENT_NAME="yuvy_bridge_linux_agent" \
+  -e TC_SERVER_URL="https://eu-west-1a.online.tableau.com/" \
+  -e SITE_NAME="ybenichou" \
+  -e USER_EMAIL="ybenichou@tableau.com" \
+  -e TOKEN_ID="yuvy_linux_bridge_pat" \
   -e TOKEN_VALUE="${TOKEN_SECRET}" \
-  -e POOL_ID="<e0739483-689f-400c-9925-e8a0f11424b3>" \
+  -e POOL_ID="3fbca586-8913-419a-8f11-cf9bbf7671e1" \
   bridge_basic
 
